05aed0c308e4e857a6a62922a4c2e3a90dd5816e
Wietse Verhagen <wietseverhagen05@gmail.com>
2026-03-12 13:08:23 +0100

    mongoDB is verbonden, en alle modules zijn geinstalleerd.

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 4000;
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+app.use(session({
+  secret: 'redbullgeeftjevleugels', // willekeurige lange zin
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: false, // Zet op true als je https gebruikt, maar op localhost is false prima
+    maxAge: 3600000 // Hoe lang de cookie geldig is (1 uur)
+  }
+}));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("static"));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+
+mongoose.connect(process.env.DB_URL);
+/////// mongo db:
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
 
 // Vertel Express dat we EJS gebruiken
 app.set('view engine', 'ejs');
