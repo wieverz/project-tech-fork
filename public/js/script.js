dd5e21a2fdab0c780f8f7ad66936a70b74161fd2
Wietse Verhagen <wietseverhagen05@gmail.com>
2026-03-19 13:35:07 +0100

    register page af? custom foutmeldingen toegevoegd

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,40 @@
-// Wacht tot de hele pagina geladen is
 document.addEventListener('DOMContentLoaded', () => {
   const rangeInput = document.getElementById('experience');
   const displayValue = document.getElementById('val');
 
-  // Check of de elementen wel op deze pagina bestaan
   if (rangeInput && displayValue) {
       rangeInput.addEventListener('input', () => {
           displayValue.innerText = rangeInput.value;
+      });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const rangeInput = document.getElementById('age');
+  const displayValue = document.getElementById('age-val');
+
+  if (rangeInput && displayValue) {
+      rangeInput.addEventListener('input', () => {
+          displayValue.innerText = rangeInput.value;
+      });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.register__form');
+  const usernameInput = document.getElementById('username');
+  const usernameError = document.getElementById('username-error');
+
+  if (form) {
+      form.addEventListener('submit', (event) => {
+          if (!usernameInput.value.trim()) {
+              event.preventDefault();
+              usernameError.classList.add('active');
+              usernameInput.classList.add('invalid');
+              // usernameInput.focus();
+          } else {
+              usernameError.classList.remove('active');
+              usernameInput.classList.remove('invalid');
+          }
       });
   }
 });
